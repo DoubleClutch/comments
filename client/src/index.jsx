dc4f1233aabeea93c10c68ad688b0067861f641e
Michael <openmichael65@gmail.com>
2018-02-22 11:21:43 -0800

    Add comment to index.js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Comments from './components/comments';
 
+//Add comment
+
 ReactDOM.render(<Comments />, document.getElementById('comments'));
