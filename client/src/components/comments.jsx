46fc5221bebc5c543d12e3b2275d70282b493ac1
Michael <openmichael65@gmail.com>
2018-02-21 17:10:35 -0800

    Setup basic react app with webpack without server

---
+++ client/src/components/comments.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Comments extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (<div>Comments!!!</div>);
+  }
+}
+
+export default Comments;
