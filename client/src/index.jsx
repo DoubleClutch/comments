2de3ba1e945daace8f7200a7499cb6b4631e8086
Michael <openmichael65@gmail.com>
2018-02-24 10:24:54 -0800

    Export components through window

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,4 +4,6 @@ import Comments from './components/comments';
 
 import '../dist/css/main.scss';
 
-ReactDOM.render(<Comments />, document.getElementById('comments'));
+// ReactDOM.render(<Comments />, document.getElementById('comments'));
+// ReactDOM.render(window.Comments, document.getElementById('comments'));
+window.Comments = Comments;
