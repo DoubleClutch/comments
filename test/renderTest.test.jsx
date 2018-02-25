08dc32616820aa47b6bb0848013b32a555665e49
Michael <openmichael65@gmail.com>
2018-02-25 10:27:26 -0800

    Change test title to Render test

--- test/renderTest.test.jsx
+++ test/renderTest.test.jsx
@@ -3,7 +3,7 @@ import Comments from '../client/src/components/comments';
 import Comment from '../client/src/components/comment';
 
 
-describe('Welcome', () => {
+describe('Render test', () => {
   it('Comments should render Comment block', () => {
     const wrapper = shallow(<Comments />);
     expect(wrapper.find(Comment).exists()).toEqual(true);
