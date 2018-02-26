ec075b3c41ee357915f60ddebddfb3d64210984a
Michael <openmichael65@gmail.com>
2018-02-26 11:26:51 -0800

    Add jest enzyme test for rendering components

--- test/renderTest.test.jsx
+++ test/renderTest.test.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import Comments from '../client/src/components/comments';
 import Comment from '../client/src/components/comment';
+import Reply from '../client/src/components/reply';
 
 
 describe('Render test', () => {
-  it('Comments should render Comment block', () => {
+  it('Comments component should render a Comment component', () => {
     const wrapper = shallow(<Comments />);
     expect(wrapper.find(Comment).exists()).toEqual(true);
+  });
+
+  it('Comment component should render comment user profile photo', () => {
+    const wrapper = shallow(<Comment />);
+    expect(wrapper.find('img')).toHaveLength(1);
+  });
+
+  it('Comment component should render comment user name', () => {
+    const wrapper = shallow(<Comment />);
+    expect(wrapper.find('.commentUserName')).toHaveLength(1);
+  });
+
+  it('Comment component should render comment user message', () => {
+    const wrapper = shallow(<Comment />);
+    expect(wrapper.find('.commentUserMessage')).toHaveLength(1);
+  });
+
+  it('Comment component should render a Reply component', () => {
+    const wrapper = shallow(<Comment />);
+    expect(wrapper.find(Reply).exists()).toEqual(true);
+  });
+
+  it('Reply component should render reply user profile photo', () => {
+    const wrapper = shallow(<Reply />);
+    expect(wrapper.find('img')).toHaveLength(1);
+  });
+
+  it('Reply component should render reply user name', () => {
+    const wrapper = shallow(<Reply />);
+    expect(wrapper.find('.replyUserName')).toHaveLength(1);
+  });
+
+  it('Reply component should render reply user message', () => {
+    const wrapper = shallow(<Reply />);
+    expect(wrapper.find('.replyUserMessage')).toHaveLength(1);
   });
 });
