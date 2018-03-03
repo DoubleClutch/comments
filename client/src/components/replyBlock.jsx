154ee1a2b22e4062b1324328246886e3d263a237
Michael <openmichael65@gmail.com>
2018-03-03 14:53:31 -0800

    Add reply button to show a reply textarea for user to input

---
+++ client/src/components/replyBlock.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class ReplyBlock extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      height: 2,
+    };
+  }
+
+  setNewHeight(event) {
+    const curHeight = Math.floor((event.target.value.length) / 20);
+    if (curHeight > 2 && curHeight !== this.state.height) {
+      this.setState({
+        height: curHeight,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="replyBlock">
+        <button>Send</button>
+        <textarea
+          rows={this.state.height}
+          placeholder="Reply..."
+          onKeyUp={this.setNewHeight.bind(this)}>
+        </textarea>
+      </div>
+    );
+  }
+}
+
+export default ReplyBlock;
