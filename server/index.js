eefbdff51321ee660c30f876a4622e58d82bde48
Michael <openmichael65@gmail.com>
2018-03-05 12:20:14 -0800

    Add cors middleware to express server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 const Database = require('../database/models/models');
 
 const app = express();
 app.use(bodyparser.json());
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use('/:id', express.static(path.join(__dirname, '../client/dist')));
