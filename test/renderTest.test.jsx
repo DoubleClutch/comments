5953c06ad4982092ff8a9f543905d1e768ff1a73
Michael <openmichael65@gmail.com>
2018-03-10 09:44:58 -0800

    Add a few render tests

--- test/renderTest.test.jsx
+++ test/renderTest.test.jsx
@@ -2,46 +2,53 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Comments from '../client/src/components/comments';
 import Comment from '../client/src/components/comment';
+import SingleComment from '../client/src/components/singleComment';
 import Reply from '../client/src/components/reply';
 
-describe('Render test', () => {
-  it('Comments component should render a Comment component', () => {
-    const wrapper = shallow(<Comments />);
-    expect(wrapper.find(Comment).exists()).toEqual(true);
+describe('Render test in comments component', () => {
+  const commentsComponent = shallow(<Comments />);
+
+  it('should render a loading text with a loading status', () => {
+    commentsComponent.setState({ status: 'loading' });
+    expect(commentsComponent.find('.loading').exists()).toEqual(true);
   });
 
-  it('Comment component should render comment user profile photo', () => {
-    const wrapper = shallow(<Comment />);
-    expect(wrapper.find('img')).toHaveLength(1);
+  it('Should render a Comment component with a done status', () => {
+    commentsComponent.setState({ status: 'done' });
+    expect(commentsComponent.find(Comment).exists()).toEqual(true);
+  });
+});
+
+describe('Render test in comments component', () => {
+  const commentComponent = shallow(<Comment comments={[1, 2, 3, 4, 5, 6]} />);
+
+  it('Should render a Single Comment component', () => {
+    expect(commentComponent.find(SingleComment).exists()).toEqual(true);
   });
 
-  it('Comment component should render comment user name', () => {
-    const wrapper = shallow(<Comment />);
-    expect(wrapper.find('.commentUserName')).toHaveLength(1);
+  it('Should render a Reply component', () => {
+    expect(commentComponent.find(Reply).exists()).toEqual(true);
   });
 
-  it('Comment component should render comment user message', () => {
-    const wrapper = shallow(<Comment />);
-    expect(wrapper.find('.commentUserMessage')).toHaveLength(1);
+  // it('Should have a handleShowMore function', () => {
+  //   expect(commentComponent.find(handleShowMore()).exists()).toEqual(true);
+  // });
+
+  const startValue = 0;
+  const endValue = 5;
+  it('should show more comments block if there are comments that are not shown', () => {
+    commentComponent.setState({
+      start: startValue,
+      end: endValue,
+    });
+    expect(commentComponent.find('.showMore').exists()).toEqual(true);
   });
 
-  it('Comment component should render a Reply component', () => {
-    const wrapper = shallow(<Comment />);
-    expect(wrapper.find(Reply).exists()).toEqual(true);
-  });
-
-  it('Reply component should render reply user profile photo', () => {
-    const wrapper = shallow(<Reply />);
-    expect(wrapper.find('img')).toHaveLength(1);
-  });
-
-  it('Reply component should render reply user name', () => {
-    const wrapper = shallow(<Reply />);
-    expect(wrapper.find('.replyUserName')).toHaveLength(1);
-  });
-
-  it('Reply component should render reply user message', () => {
-    const wrapper = shallow(<Reply />);
-    expect(wrapper.find('.replyUserMessage')).toHaveLength(1);
+  const newEndValue = 6;
+  it('should not show more comments block if there are no more comments that can be shown', () => {
+    commentComponent.setState({
+      end: newEndValue,
+    });
+    expect(commentComponent.find('.showMore').exists()).toEqual(false);
   });
 });
